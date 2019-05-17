0fc9707a3d98786681dfb6ac8c8cad4ac53eb947
bastienchassagnol <bastien.chassagnol@insa-lyon.fr>
2019-05-17 14:36:38 +0200

    Update hpc.R

--- R/hpc.R
+++ R/hpc.R
@@ -79,8 +79,8 @@ hybrid.pc.global.cluster = function(
 hybrid.pc = function(t, data, nodes, whitelist, blacklist, test, alpha,
                      test.args, pc.method, backtracking = NULL, debug = FALSE) {
   #check temporaly special node
-  print("hello, je suis là")
-  cat ("node is",typeof(t))
+  
+ 
   if (t=="VTUB") {
     debug=TRUE
     }
