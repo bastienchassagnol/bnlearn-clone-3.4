ff5aa2e2ac1f7443d82685be85ef3d9a72d35bb4
bastienchassagnol <bastien.chassagnol@insa-lyon.fr>
2019-05-21 15:19:48 +0200

    Update iambfdr.R

--- R/iambfdr.R
+++ R/iambfdr.R
@@ -247,7 +247,7 @@ iambfdr = function(x, data, nodes, alpha, test.args, whitelist, blacklist,
     break
 
   }#REPEAT
-  cat ("final mb found for node ",t," is ",mb)
+  cat ("final mb found for node ",t," is ",mb,"\n\n")
   return(mb)
 
 }#IAMBFDR
